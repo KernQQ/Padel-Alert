446b37e840fcdc6dfa3c5d394261f5728d927ff4
Mariusz <mariuszbudziszewski96@gmail.com>
2026-09-05 19:01:06 +0200

    Padletic mobile 1to1 update 02

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import "./styles/padletic-12.css";
 import "./styles/mobile-stability.css";
 import "./styles/full-update.css";
 import "./styles/sport-system.css";
+import "./styles/mobile-1to1.css";
 
 import { API_URL, REFRESH_SECONDS, DURATIONS, NAVIGATION } from "./config/app";
 import {
