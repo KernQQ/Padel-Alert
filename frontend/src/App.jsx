4858140c6e676126360225d2d77ff52e395d5750
Mariusz <mariuszbudziszewski96@gmail.com>
2026-09-05 22:28:00 +0200

    Padletic desktop clean reset 10

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import "./styles/sport-system.css";
 import "./styles/mobile-1to1.css";
 import "./styles/mobile-platform-fix.css";
 import "./styles/desktop-v2.css";
-
+import "./styles/desktop-clean-reset.css";
 import { API_URL, REFRESH_SECONDS, DURATIONS, NAVIGATION } from "./config/app";
 import {
   getToday,
